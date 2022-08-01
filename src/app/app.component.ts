a1c5c02d26ead4782ddf3902bdf85ed9022b184a
Sabrina <54827482+sabrinaspage@users.noreply.github.com>
2022-08-01 05:37:15 -0400

    first angular todo list 🍓

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'sabrinas-portfolio';
+  public items: string[] = [];
+  public newTask: string = '';
+
+  public addToList() {
+    if (this.newTask == '') {
+    } else {
+      this.items.push(this.newTask);
+      this.newTask = '';
+    }
+  }
+
+  public deleteTask(index: number) {
+    this.items.splice(index, 1);
+  }
 }
